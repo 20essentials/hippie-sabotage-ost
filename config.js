b135da024fe6551cfb37ae818547278e934cac87
DominicDev <s0l0estudi4r@gmail.com>
2025-10-03 15:50:34 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'HIPPIE SABOTAGE';
+export const nameSongs = [
+	"DEVIL EYES", /*1*/
+	"TURTLE IN THE WAVES", /*2*/
+	"DON’T TEMPT ME", /*3*/
+	"DEVIL EYES (GOLDEN)", /*4*/
+	"CARDS I’M DEALT", /*5*/
+	"NO JUDGEMENT", /*6*/
+	"DUMB LUCK", /*7*/
+	"FORTUNE TELLER", /*8*/
+	"ASSASSIN IN THE WINDOW", /*9*/
+	"YOU'RE SO FUCKING MONEY", /*10*/
+	"SHIPWRECKED", /*11*/
+	"HOMEGROWN", /*12*/
+	"HIT YOU BACK TOMORROW", /*13*/
+	"RUNNAWAY", /*14*/
+	"LIFE ON THE ISLANDS", /*15*/
+	"SNAP OUT OF IT", /*16*/
+	"I DON’T CARE", /*17*/
+	"ALL AT ONCE", /*18*/
+	"SUN UP", /*19*/
+	"MIDNIGHT OWL", /*20*/
+]
